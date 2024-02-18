82fd34e5a0afd5d7a0003e4cb85581558b1ada90
Sajath Firthows <106432527+sajathfirthows@users.noreply.github.com>
2024-02-18 11:00:42 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
-resource "aws_subnet" "mysubnet" {
+resource "aws_subnet" "subet-saj" {
   count             = 3
   vpc_id            = aws_vpc.my-saja-vpc.id
   cidr_block        = element(var.subnet_cidr_blocks, count.index)
-  availability_zone = "us-east-1a"
+  availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
 }
